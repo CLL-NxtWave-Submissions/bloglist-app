b55fa9ddce761a674520f3eefdcd916d2f00ca2b
Surya T N <suryatej.nim@yahoo.com>
2022-10-04 07:52:19 +0000

    Added CSS styling for JSX elements and implemented the intended UI to be rendered with BlogList component

--- src/components/BlogList/index.js
+++ src/components/BlogList/index.js
@@ -1,1 +1,16 @@
-// Write your JS code here
+import BlogItem from '../BlogItem'
+import './index.css'
+
+const BlogList = props => {
+  const {listData} = props
+
+  return (
+    <ul className="blog-list-container">
+      {listData.map(listDataItem => (
+        <BlogItem key={listDataItem.id} itemData={listDataItem} />
+      ))}
+    </ul>
+  )
+}
+
+export default BlogList
