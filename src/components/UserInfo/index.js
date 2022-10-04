e8c2abee32d533f77ffe278f6734234aa12a17dd
Surya T N <suryatej.nim@yahoo.com>
2022-10-04 07:41:46 +0000

    Added CSS styling for JSX elements in UserInfo component and implemented the intended UI to be rendered

--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -1,1 +1,15 @@
-// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      className="user-profile-img"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+    />
+    <h1 className="user-profile-name">Wade Warren</h1>
+    <p className="user-profile-description">Software Developer at UK</p>
+  </div>
+)
+
+export default UserInfo
